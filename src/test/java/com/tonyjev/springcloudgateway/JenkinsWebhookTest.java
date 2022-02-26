6a6e079194fc25ba28fda62df311199afc732b9a
JUN GYEWON <tonyjev93@gmail.com>
2022-02-26 22:47:15 +0900

    chore: webhook test

---
+++ src/test/java/com/tonyjev/springcloudgateway/JenkinsWebhookTest.java
@@ -0,0 +1,15 @@
+package com.tonyjev.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+
+public class JenkinsWebhookTest {
+    @Test
+    public void test() throws Exception {
+        // given
+
+        // when
+
+        //then
+
+    }
+}
